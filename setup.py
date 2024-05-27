798f7ef564b80b8d86023dbc78feeecd1f6c721a
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-27 15:58:09 +0200

    Requirements and setup.py are created

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+with open("requirements.txt") as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(
+    name="predictor",
+    description="startup success predictor package",
+    packages=find_packages(),
+    install_requires=requirements
+)
