7424e4a8e20f13869c9a4d95f1d8fb02ff5a2503
Berkan Cetin <m.berkancetin@icloud.com>
2024-05-30 23:24:41 +0200

    main.py checkpoint

--- predictor/interface/main.py
+++ predictor/interface/main.py
@@ -11,3 +11,18 @@ def pred(X_pred: pd.DataFrame = None) -> np.ndarray:
         returns to a numpy array.
     """
     pass
+
+if __name__ == '__main__':
+    try:
+        # preprocess_and_train()
+        # preprocess()
+        # train()
+        pred()
+    except:
+        import sys
+        import traceback
+
+        import ipdb
+        extype, value, tb = sys.exc_info()
+        traceback.print_exc()
+        ipdb.post_mortem(tb)
